ae0a12ebcff3ccf811a373063d1d0bb36f3bd68b
zebibu alemayew beyene <felekebetty8@gmail.com>
2025-09-08 01:36:48 +0200

    small change in header

--- AMAZON-CLONE/src/components/Header/LowerHeader.jsx
+++ AMAZON-CLONE/src/components/Header/LowerHeader.jsx
@@ -1,20 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiOutlineMenu } from "react-icons/ai";
 import "./LowerHeader.css";
 
 function LowerHeader() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+
   return (
     <div className="lower-header">
-      <ul>
-        <li>
+      <ul className={`menu-list ${menuOpen ? "open" : ""}`}>
+        {/* Always visible */}
+        <li className="all-menu" onClick={toggleMenu}>
           <AiOutlineMenu />
           <p>All</p>
         </li>
-        <li>Today's Deals</li>
-        <li>Customer Service</li>
-        <li>Registry</li>
-        <li>Gift Cards</li>
-        <li>Sell</li>
+
+        {/* Hidden on mobile by default */}
+        <li className="menu-item">Today's Deals</li>
+        <li className="menu-item">Customer Service</li>
+        <li className="menu-item">Registry</li>
+        <li className="menu-item">Gift Cards</li>
+        <li className="menu-item">Sell</li>
       </ul>
     </div>
   );

037ed9b632faa6d6dedeef65160c27375ae1acf0
zebibu alemayew beyene <felekebetty8@gmail.com>
2025-09-13 13:44:09 +0200

    Update index.js

--- AMAZON-CLONE/functions/index.js
+++ AMAZON-CLONE/functions/index.js
@@ -1,25 +1,36 @@
-const {onRequest} = require("firebase-functions/https");
+const { onRequest } = require("firebase-functions/https");
 const logger = require("firebase-functions/logger");
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
-const stripe = require("stripe")(
-  process.env.STRIPE_KEY
-);
-
-
+const stripe = require("stripe")(process.env.STRIPE_KEY);
 
 const app = express();
-app.use(cors({origin: true}));
+app.use(cors({ origin: true }));
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.status(200).json({
-        message: "Success !"
-    });
-})
+  res.status(200).json({
+    message: "Success !",
+  });
+});
 
+app.post("/payments/create", async (req, res) => {
+  const total = req.query.total;
+
+  if (total > 0) {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: total,
+      currency: "usd",
+    });
+    res.status(201).json({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } else {
+    res.status(400).json({ error: "Invalid amount" });
+  }
+});
 
 exports.api = onRequest(app);
